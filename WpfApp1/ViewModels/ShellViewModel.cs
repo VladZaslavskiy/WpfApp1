00c7b1bfc12e242c66512a9109536ca8dd557b5d
VladZaslavskiy <vladislav.zaslavskyi3@gmail.com>
2021-11-24 11:47:24 +0200

    Add project files.

---
+++ WpfApp1/ViewModels/ShellViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Caliburn.Micro;
+
+namespace WpfApp1.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        public void LoadPageOne()
+        {
+            ActivateItemAsync(new PageOneViewModel());
+        }
+
+        public void LoadPageTwo()
+        {
+            ActivateItemAsync(new PageTwoViewModel());
+        }
+    }
+}
